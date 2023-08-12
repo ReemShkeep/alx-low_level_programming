9c06072a874aae0cb2121812d045f8040d65fec3
ReemShkeep <eng.reemshkeep@gmail.com>
2023-08-11 18:34:46 -0700

    size of correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,12 @@
 
 int main(void)
 {
-printf("size of a char: %d byte(s)", sizeof(char));
-printf("size of an int: %d byte(s)", sizeof(int));
-printf("size of a long int: %d byte(s)", sizeof(long int));
-printf("size of a long long int: %d byte(s)", sizeof(long long int));
-printf("size of a float: %d byte(s)", sizeof(float));
+printf("Size of a char: %d byte(s) \n ", sizeof(char));
+printf("Size of an int: %d byte(s) \n ", sizeof(int));
+printf("Size of a long int: %d byte(s) \n ", sizeof(long int));
+printf("Size of a long long int: %d byte(s) \n ", sizeof(long long int));
+printf("Size of a float: %d byte(s) \n ", sizeof(float));
 return(0);
 }
+
+
